35fade03b43a4979bf42caa4bcee66ed1fe867cd
eden lane <edencore@gmail.com>
2013-09-03 15:50:34 +0000

    Reorganaizing

---
+++ paging.js
@@ -0,0 +1,48 @@
+/*
+ * @author eden lane <edencore@gmail.com>
+ */
+ 
+paging = function(params) {
+    if (typeof params.current == 'undefined')
+        throw new Error("parameter 'current' is required");
+    if (typeof params.total == 'undefined')
+        throw new Error("parameter 'total' is required");
+        
+    var current = params.current,
+        total = params.total,
+        max = params.max || 5,
+        url = params.url;
+    
+    var result = [];
+        side = Math.floor(max / 2),
+        start = current - side,
+        finish = current + side;
+    
+    var d = finish - total;
+	if (d > 0) {
+		start -= d;
+		finish -= d;
+	}
+	if (start <= 0) {
+		d = 1 - start;
+		start += d;
+		finish += d;
+	}
+	if (finish > total) {
+		finish = total;
+	}
+        
+    for (var i = start; i <= finish; i++) {
+        var page = {
+            id: i,
+            isCurrent: i == current
+        };
+        if (url) {
+            page.url = url + i;
+        }
+        result.push(page);
+    }
+    return result;
+};
+
+module.exports = paging;
